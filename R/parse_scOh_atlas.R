c7bd7527001d40feb7c044309edc1b4914672498
lthealy <luke.torrehealy@stonybrookmedicine.edu>
2022-06-06 21:03:40 +0000

    added patient level tumor/normal calls

--- R/parse_scOh_atlas.R
+++ R/parse_scOh_atlas.R
@@ -1,6 +1,7 @@
 #' This code is here for transparency, but for data storage reasons the full combined rds file is not available.
 #'
 #' @import Seurat
+#' @import readxl
 #' @export
 #'
 
@@ -8,8 +9,8 @@ parse_scOh_atlas <- function(){
   ## Won't be available as is, here for transparency and posterity
   atlas <- readRDS("/premDisk/khoh/Github/scTME/data/pdac_full_combined.rds")
   atlas@meta.data = atlas@meta.data[,which(colnames(atlas@meta.data) %in%
-                                         c("Patient",
-                                           "Dataset")
+                                             c("Patient",
+                                               "Dataset")
   )]
   atlas@meta.data$Patient[which(is.na(atlas$Patient))] = "Powers1"
   tmp_converted = Convert_GUI_data(atlas, from = "Seurat", to ="Moffitt",
@@ -17,26 +18,17 @@ parse_scOh_atlas <- function(){
   tmp_converted$metadata$log.transformed = FALSE
   rownames(tmp_converted$sampInfo) = NULL
 
-  tmp$sampInfo = dplyr::left_join(tmp$sampInfo, SupplementaryTable4Metadata, by = "Patient")
-  tmp$sampInfo = tmp$sampInfo[,order(colnames(tmp$sampInfo))]
+  SupplementaryTable4Metadata <- read_excel("inst/extdata/scAtlas/SupplementaryTable4Metadata.xlsx")
+
+  tmp_converted$sampInfo = dplyr::left_join(tmp_converted$sampInfo, SupplementaryTable4Metadata, by = "Patient")
+  tmp_converted$sampInfo = tmp_converted$sampInfo[,order(colnames(tmp_converted$sampInfo))]
+  tmp_converted$sampInfo = tmp_converted$sampInfo[,c(which(colnames(tmp_converted$sampInfo) == "Patient"),
+                                                     which(colnames(tmp_converted$sampInfo) != "Patient"))]
+  tmp_converted$metadata$default_selections = list(filter_column = "Condition",
+                                                   filter_levels = "Normal",
+                                                   sampleTracks = "Dataset")
   saveRDS(tmp_converted,
           file = "./data/scAtlas.pseudobulked.rds",
           compress = T)
 
-  ## If we want to save dataset level objects
-  # for(i in levels(as.factor(atlas$Dataset))){
-  #   cells = which(atlas$Dataset == i)
-  #   tmp = subset(atlas, cells = cells)
-  #   tmp@meta.data = tmp@meta.data[,which(colnames(tmp@meta.data) %in%
-  #                                         c("Patient",
-  #                                           "Dataset")
-  #                                       )]
-  #   tmp_converted = Convert_GUI_data(atlas, from = "Seurat", to ="Moffitt",
-  #                                    pseudobulk = T, bulk_by = "Patient")
-  #   tmp_converted$metadata$log.transformed = FALSE
-  #   rownames(tmp_converted$sampInfo) = NULL
-  #   saveRDS(tmp_converted,
-  #           file = paste0("/datadrive/shared/pdacR_datasets/",i,".pseudobulked.rds"),
-  #           compress = T)
-  # }
 }
